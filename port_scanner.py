8d73862fb2635c3e10f90a3f54367a18a45d39f6
Jek08 <58660820+Jek08@users.noreply.github.com>
2019-12-20 21:40:31 +0700

    Add files via upload

---
+++ port_scanner.py
@@ -0,0 +1,43 @@
+import socket as s
+import threading
+
+
+def check_port_connection(ip, port_number, ports):
+    """
+    Check which port is open
+    """
+    socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+    socket.settimeout(5)
+    is_connected = socket.connect_ex((ip, port_number))
+    if is_connected == 0:
+        ports[port_number] = 'open'
+    else:
+        ports[port_number] = ''
+
+
+def scan_port(ip):
+    """
+    Run thread on check_port_connection() simultaneously.
+    Then print the open port after each thread has finished checking.
+    """
+    ports = dict()
+    threads = list()
+
+    for i in range(10000):
+        t = threading.Thread(target=check_port_connection, args=(ip, i, ports))
+        threads.append(t)
+        t.start() # start each thread
+
+    for i in range(10000):
+        threads[i].join()
+
+        if ports[i] is "open":
+            print("Port %s : open." % i)
+
+
+if __name__ == "__main__":
+    target = input("Enter target hostname: ")
+    target_ip = s.gethostbyname(target)
+    print("Targeting on: %s" % target_ip)
+
+    scan_port(target_ip)  # initiate the scan
